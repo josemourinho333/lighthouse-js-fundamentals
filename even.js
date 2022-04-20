debcf6262adb88505a59099062cb89aea711d844
Phil Y <yoo.phil92@gmail.com>
2022-04-20 09:27:34 -0700

    Is a number I pass even?

---
+++ even.js
@@ -0,0 +1,13 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+//this step is not needed
+// const tenIsEven = isEven(10);
+// const elevenIsEven = isEven(11);
+// console.log(tenIsEven);
+// console.log(elevenIsEven);
+
+//Shorter way
+console.log(isEven(10));
+console.log(isEven(11));
