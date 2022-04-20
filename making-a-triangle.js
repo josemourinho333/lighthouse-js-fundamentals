117d21336679cb0f5edccef102122ea728558a72
Phil Y <yoo.phil92@gmail.com>
2022-04-19 22:15:39 -0700

    Building a Triangle Exercise

---
+++ making-a-triangle.js
@@ -0,0 +1,20 @@
+// creates a line of * for a given length
+function makeLine(length) {
+  let line = "";
+  for (let j = 1; j <= length; j++) {
+      line += "* ";
+  }
+  return line + "\n";
+}
+
+function buildTriangle(length) {
+  let triangle = '';
+  let lineNumber = 1;
+
+  for (lineNumber = 1; lineNumber <= length; lineNumber++) {
+    triangle = triangle + makeLine(lineNumber);  
+  }
+  return triangle;
+}
+
+console.log(buildTriangle(5));
