fae8673cd754489ad3376e8afd41d3fb6efdfd00
Phil Y <yoo.phil92@gmail.com>
2022-04-20 13:25:13 -0700

    Maple Syrup Containers

---
+++ how-many-hundreds.js
@@ -0,0 +1,11 @@
+const howManyHundreds = function (num) {
+  let containers = num / 100;
+  containers = Math.floor(containers);
+  return containers;
+};
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
