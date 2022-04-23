1cd1c80dc5939f538b95899d55863bd685bffb4c
Phil Y <yoo.phil92@gmail.com>
2022-04-23 11:52:09 -0700

    Final passing code

--- judge-veggies.js
+++ judge-veggies.js
@@ -1,36 +1,54 @@
 const vegetables = [
   {
-    submitter: 'Old Man Franklin',
-    redness: 10,
-    plumpness: 5
+    submitter: 'A',
+    d: 5,
   },
   {
-    submitter: 'Sally Tomato-Grower',
-    redness: 2,
-    plumpness: 8
+    submitter: 'B',
+    d: 10,
   },
   {
-    submitter: 'Hamid Hamidson',
-    redness: 4,
-    plumpness: 3
+    submitter: 'C',
+    d: 25,
   }
 ]
 
-const metric = 'redness'
+//differet tests
+// const vegetables = [
+//   {
+//     submitter: 'Old Man Franklin',
+//     redness: 10,
+//     plumpness: 5
+//   },
+//   {
+//     submitter: 'Sally Tomato-Grower',
+//     redness: 2,
+//     plumpness: 8
+//   },
+//   {
+//     submitter: 'Hamid Hamidson',
+//     redness: 4,
+//     plumpness: 3
+//   }
+// ]
+
+const metric = 'd'
+
 
 const judgeVegetable = function (vegetables, metric) {
-  if (metric === 'redness') {
+  let measure = metric;
+  if (measure === metric) {
     let scores = [];
     for (let i = 0; i < vegetables.length; i++) {
-      scores.push(vegetables[i].redness);
-    };
+      scores.push(vegetables[i][metric]);
+    }
     let winner = Math.max(...scores);
     for (let y = 0; y < vegetables.length; y++) {
-      if (winner === vegetables[y].redness) {
+      if (winner === vegetables[y][metric]) {
         let winnerName = vegetables[y].submitter;
         return winnerName;
-      };
-    };
+      }
+    }
   } else {
     return undefined;
   }
